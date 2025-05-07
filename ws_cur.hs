c2a64e006bb0822099b2cfb4f9ccca9585597dcc
burkitkahu <burkitkahu@myvuw.ac.nz>
2025-05-07 17:51:17 +1200

    all casting working and type identification

--- ws_cur.hs
+++ ws_cur.hs
@@ -1,35 +1,94 @@
 module Main where
 import System.Environment
-import System.FilePath (takeFileName)
 import System.IO
 import Control.Monad
 import Data.Bits (xor, shiftL, shiftR, complement)
 import Data.Char (isSpace, isDigit, toLower)
 import Data.Fixed (mod')
-import Data.List (transpose, intercalate)
+import Data.List (isPrefixOf, isSuffixOf, intercalate, transpose)
+import Text.Read (readMaybe)
+import Data.Maybe (fromMaybe)
 
-data Value = VInt Int | VFloat Float | VBool Bool | VStr String | VVector [Int] | VMatrix [[Int]]
-            | VQuoting String | VLambda String
-  deriving (Eq)
-instance Show Value where
-  show (VInt i)  = show i
-  show (VFloat f)  = show f
-  show (VBool b) = map toLower (show b)
-  show (VStr s)  = "\"" ++ s ++ "\""
-  show (VVector v) = formatList v
-  show (VMatrix m) = "[" ++ intercalate ", " (map formatList m) ++ "]"
-  show (VQuoting q) = q
-  show (VLambda l) = l
+data Node = IntNode Int
+          | OpNode String
+          | FloatNode Float
+          | BoolNode Bool
+          | StrNode String
+          | VectorNode [Int]
+          | MatrixNode [[Int]]
+          | QuoteNode String
+          | LambdaNode String
+          deriving (Eq)
+
+instance Show Node where
+  show (OpNode op)    = show op
+  show (IntNode i)    = show i
+  show (FloatNode f)  = show f
+  show (BoolNode b)   = map toLower (show b)
+  show (StrNode s)    = "\"" ++ s ++ "\""
+  show (VectorNode v) = formatList v
+  show (MatrixNode m) = "[" ++ intercalate ", " (map formatList m) ++ "]"
+  show (QuoteNode q)  = q
+  show (LambdaNode l) = l
+
+-- helper stuff for printing TODO remove
+showNodeType :: Node -> String
+showNodeType (IntNode _)    = "Int"
+showNodeType (FloatNode _)  = "Float"
+showNodeType (BoolNode _)   = "Bool"
+showNodeType (StrNode _)    = "String"
+showNodeType (VectorNode _) = "Vector"
+showNodeType (MatrixNode _) = "Matrix"
+showNodeType (QuoteNode _)  = "Quote"
+showNodeType (LambdaNode _) = "Lambda"
+showNodeType (OpNode _) = "Operator"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  contents <- readFile (head args)
-  let input = head args
-      inputFile = takeFileName input
-      tokens = tokenize contents "" False False 0
-  mapM_ putStrLn tokens
--- Your exact tokenize function (unchanged as requested)
+        args <- getArgs
+        contents <- readFile (head args)
+        let tokens = tokenize contents "" False False 0
+            castedTokens = map castToken tokens
+
+        mapM_ (\node -> putStrLn $ "Token: " ++ show node ++ " :: " ++ showNodeType node) castedTokens
+
+castToken token
+  | isLambda token     = LambdaNode token
+  | isQuoted token     = QuoteNode (tail token)
+  | isMatrix token     = MatrixNode (parseMatrix token)
+  | isVector token     = VectorNode (parseVector token)
+  | isString token     = StrNode (init (tail token))
+  | isBool token       = BoolNode (toBool token)
+  | isInteger token    = IntNode (read token)
+  | isFloat token      = FloatNode (read token)
+  | isOp token         = OpNode token
+  | otherwise          = StrNode token
+  where
+    isLambda s = "{" `isPrefixOf` s && "}" `isSuffixOf` s
+    isQuoted s = "'" `isPrefixOf` s
+    isMatrix s = "[[" `isPrefixOf` s && "]]" `isSuffixOf` s
+    isVector s = "[" `isPrefixOf` s && "]" `isSuffixOf` s && not (isMatrix s)
+    isString s = "\"" `isPrefixOf` s && "\"" `isSuffixOf` s
+    isBool s = case map toLower s of
+                   "true"  -> True
+                   "false" -> True
+                   _       -> False
+    isOp s = s `elem` [ "+", "-", "*", "/", "%", "**", "x", "==", "!=", "<", ">", "<=", ">=", "<=>", "&", "|", "^", "<<", ">>", "!", "~", "DROP", "DUP", "SWAP", "ROT", "ROLL", "ROLLD", "IFELSE", "TRANSP", "EVAL"]
+    isInteger s = case reads s :: [(Int, String)] of [(_, "")] -> True; _ -> False
+    isFloat s = case reads s :: [(Float, String)] of [(_, "")] -> True; _ -> False
+
+    toBool s = case map toLower s of
+                 "true"  -> True
+                 "false" -> False
+-- Helpers to safely parse vector/matrix
+parseVector :: String -> [Int]
+parseVector s = fromMaybe [] (readMaybe s :: Maybe [Int])
+
+parseMatrix :: String -> [[Int]]
+parseMatrix s = fromMaybe [] (readMaybe s :: Maybe [[Int]])
+
+
+-- TODO : ADJUST THIS
 tokenize :: [Char] -> String -> Bool -> Bool -> Int -> [String]
 tokenize [] s _ _ _ 
     | null s = []
